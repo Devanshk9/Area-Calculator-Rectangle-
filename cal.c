59671d74c04558ffd0ab0c4260f78ecf91e26c22
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2023-06-29 18:55:21 +0530

    Create cal.c

---
+++ cal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    float a,b ;
+    printf("Enter the value of length and breadth \n");
+    scanf("%f %f", &a, &b );
+    printf("The Area of Rectangle = %f", a*b);
+
+
+    return 0;
+}
